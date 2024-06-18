5b621086c3b709bae6c57496b03dd1c3ea296a61
Rohit Kumar <rohityadav.se@gmail.com>
2024-06-18 15:45:47 +0545

    Update app.js listen address to use port 3000

--- app.js
+++ app.js
@@ -37,6 +37,6 @@ app.use((err, req, res, next) => {
     }
 })
 
-app.listen(process.env.PORT, () => {
-    console.log(`Listening on port ${process.env.PORT}`)
+app.listen(3000, () => {
+    console.log(`Listening on port 3000`)
 });
